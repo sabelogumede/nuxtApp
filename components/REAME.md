93a11f59641d29ac6f3e8ae5ff0b87fad1ce760c
sabelogumede <iamsabelogumede@gmail.com>
2019-01-22 13:19:00 +0200

    my nux.js recipe app initialization

---
+++ components/REAME.md
@@ -0,0 +1,7 @@
+#COMPONETS
+
+*This directory is not required, you can delete it if you don't want to use it.**
+
+The components directory contains your Vue.js Components.
+
+_Nux.js doesn't supercharge these componets._
---
+++ pages/recipes/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="recipes">
+
+        <!-- first recipe -->
+        <Recipe 
+            thumbnail="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/2/19/1/FN_gluten-free-mac-n-cheese_s4x3.jpg.rend.hgtvcom.616.462.suffix/1393442138933.jpeg" 
+            title="Delicious"
+            previewText="this is a descriptive text area" />
+
+        <!-- second recipe -->
+        <Recipe 
+            thumbnail="http://images.media-allrecipes.com/images/74681.jpg" 
+            title="Delicious"
+            previewText="this is a descriptive text area" />
+
+        <!-- third recipe -->
+        <Recipe 
+            thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQqXknXshTE2WXMoy6KhmRRlqg0Oeet0Y9Ao6onD-HdoH0AL7qLA" 
+            title="Delicious"
+            previewText="this is a descriptive text area" />
+
+        <!-- forth recipe -->
+        <Recipe 
+            thumbnail="https://www.primaverakitchen.com/wp-content/uploads/2018/12/Low-Carb-Shrimp-Fajita-Meal-Prep-Bowls-Primavera-Kitchen-3-500x660.jpg" 
+            title="Delicious"
+            previewText="This is a descriptive text area" />
+
+    </section>
+</template>
+
+// js
+// registering our recipe import component
+<script>
+import Recipe from '@/components/Recipe'
+
+export default {
+    components: {
+        Recipe
+    } 
+}
+
+</script>
+
+// css
+<style scoped>
+    .recipes {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
